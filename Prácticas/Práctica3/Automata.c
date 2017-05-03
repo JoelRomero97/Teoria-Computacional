8f430f037e14dc048ba060e62fc48a6f09131143
Joel Romero <joelrg1288@gmail.com>
2017-05-02 23:56:22 -0500

    Autómata que verifica la expresión regular (1|0)*11001* además de mostrar su quintupla.

---
+++ Prácticas/Práctica3/Automata.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 100
+
+void pedirQuintupla();
+void pedirCadenas(char expresion[]);
+int validarCadena(char cadena []);
+
+int main()
+{
+	pedirQuintupla();
+}
+
+void pedirQuintupla()
+{
+	int nEstados,nSimbolos,otra = 0,nTransicion = 1, i = 0;
+	char exp[TAM],transicion[TAM];
+	char *pt = transicion;
+	char estadoFinal,estadoInicial;
+	printf("\n\nIngrese el numero de simbolos:\t");
+	scanf("%d",&nSimbolos);
+	fflush(stdin);
+	printf("\n\nIngrese el estado inicial:\t");
+	scanf("%c",&estadoInicial);
+	fflush(stdin);
+	printf("\n\nIngrese el estado final:\t");
+	scanf("%c",&estadoFinal);
+	fflush(stdin);
+	while(otra < 9)
+	{
+		system("cls");
+		printf("Transicion numero %d",nTransicion++);
+		printf("\n\nIngrese estado origen:\t");
+		scanf("%c",&transicion[i++]);
+		fflush(stdin);
+		printf("Ingrese el simbolo:\t");
+		scanf("%c",&transicion[i++]);
+		fflush(stdin);
+		printf("Ingrese estado destino:\t");
+		scanf("%c",&transicion[i++]);
+		fflush(stdin);
+		otra++;
+	}
+	transicion[i] = '\0';
+	pedirCadenas(transicion);
+}
+
+void pedirCadenas(char expresion[])
+{
+	int nCadena = 1, opc;
+	char cadena[TAM];
+	while(opc!=2)
+	{
+		system("cls");
+		printf("Ingrese la cadena %d:\t",nCadena++);
+		scanf("%s",&cadena);
+		validarCadena(cadena);
+		printf("\n\n%cDesea ingresar otra cadena?",168);
+		printf("\n\n1.SI\t\t2.NO\n\n");
+		scanf("%d",&opc);
+	}
+}
+
+int validarCadena(char cadena [])
+{
+	char *pt = cadena;
+	int flag;
+	VALIDAR:
+	while(*pt!='\0')
+	{
+		if(*pt=='1')
+		{
+			if(*(pt+1)=='1')
+			{
+				if(*(pt+2)=='0')
+				{
+					if(*(pt+2)=='0')
+					{
+						pt++;pt++;pt++;pt++;
+						goto KLEENE;
+					}
+				}
+			}
+		}
+		pt++;
+	}
+	KLEENE:
+	if(*pt!='\0')
+	{	
+		while(*pt!='\0')
+		{
+			if(*pt!='1')
+			{
+				flag = 0;
+			}else
+			{
+				flag = 1;
+			}
+			pt++;
+		}
+		if(flag == 1)
+		{
+			printf("\n\nCadena valida");
+		}else
+		{
+			printf("\n\nCadena Invalida");
+		}
+	}else
+	{
+		printf("\n\nCadena valida");
+	}
+}
+
+
+
